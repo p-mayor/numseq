4081e1adc0fe97d085db8b0ebb3ad6329c4783ce
Peter <peter@Peters-MBP-2.localdomain>
2019-06-17 16:44:33 -0400

    works but is_prime runs slowly

---
+++ numseq/geo.py
@@ -0,0 +1,13 @@
+def square(n):
+    return n*n
+
+
+def triangle(n):
+    output = 0
+    for i in range(0, n):
+        output += i+1
+    return output
+
+
+def cube(n):
+    return n*n*n
---
+++ numseq/prime.py
@@ -0,0 +1,18 @@
+import math
+# ref: https://stackoverflow.com/questions/11619942/print-series-of-prime-numbers-in-python
+
+
+def primes(n):
+    primes = [2]
+    for num in range(3, n, 2):
+        if all(num % i != 0 for i in range(2, int(math.sqrt(num))+1)):
+            primes.append(num)
+    print(primes)
+    return primes
+
+
+def is_prime(m):
+    if m in primes(m+1):
+        return True
+    else:
+        return False
